76c864f16b4af991075f84056a4724cab67f3cdd
yashviradia <y.viradia@tu-braunschweig.de>
2023-06-19 11:10:59 +0200

    connected to front end successful

--- server.js
+++ server.js
@@ -1,111 +1,38 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  // connect to your own database here:
+  client: 'pg',
+  connection: {
+    host : 'localhost',
+    port : 5433,
+    user : 'postgres',
+    password : 'yashviradia',
+    database : 'smart-brain'
+  }
+});
+
 const app = express();
-const port = 3001;
 
-const knex = require('knex')
+app.use(cors())
+app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
-const saltRounds = 10;
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-const smartbrain_db = knex({
-    client: 'pg',
-    
-});
-
-smartbrain_db.select('*').from('users').then(data => {
-    console.log(data);
-});
-  
-
-app.use(bodyParser.json()) // for parsing application/json
-app.use(cors());
-
-
-
-app.get('/', (req, res) => {
-    res.json(database.users);
-});
-
-app.post('/signin', (req, res) => {
-    smartbrain_db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid) {
-                return smartbrain_db.select('*').from('users')
-                .where('email', '=', req.body.email)
-                .then(user => {
-                    res.json(user[0])
-                })
-                .catch(err => res.status(400).json("unable to get user."));
-            } else {
-                console.log(data);
-                res.status(400).json("wrong credentials.");
-            }
-        })
-        .catch(err => res.status(400).json("wrong credentials"));
-});
-
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hash = bcrypt.hashSync(password, salt);
-    
-    smartbrain_db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email 
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0]["email"],
-                name: name,
-                joined: new Date
-            })
-            .then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    })
-    .catch(err => res.status(400).json("Unable to register."));
-    
-    
-});
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    let found = false;
-    smartbrain_db.select('*').from('users').where({
-        id: id
-    })
-     .then(user => {
-        if (user.length) {
-            res.json(user[0]);
-        } else {
-            res.status(400).json("Not found");
-        }
-     })
-     .catch(err => res.status(400).json("error getting user."))
-});
-
-app.put('/image', (res, req) => {
-    const { id } = req.body;
-    smartbrain_db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0])
-    })
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
